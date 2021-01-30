4fb2169cf1541ec2d8b1c35ee804558da5a3ccb0
John <ideanitypro@gmail.com>
2021-01-30 19:21:54 +0000

    Added endpoint for creating authenticated users

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'rest_framework',
+    'rest_framework.authtoken',
     # Custom Apps
     'core',
+    'user',
 ]
 
 AUTH_USER_MODEL = 'core.User'
